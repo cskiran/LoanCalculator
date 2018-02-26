160b1fdc62dbb3abfb211b4c6d73660a7e43718f
unknown <cskiran3904@gmail.com>
2018-02-26 14:13:20 +0530

    added the loader for user experience

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function(e){
+  //Hide loader
+  document.getElementById('results').style.display = 'none';
+  //show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults,2000);
+
+  e.preventDefault();
+});
 
 
-function calculateResults(e){
+function calculateResults(){
   console.log('calculating...')
   //UI variables
   const UIamount = document.getElementById('amount');
@@ -25,14 +34,20 @@ function calculateResults(e){
   if(isFinite(monthly)){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatePayments).toFixed(2);
-    totalInterest.value = ((monthly*calculatePayments)-principle).toFixed(2)
+    totalInterest.value = ((monthly*calculatePayments)-principle).toFixed(2);
+
+    //show results
+    document.getElementById('results').style.display = 'block';
+
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
   }else{
     showError('Please check your numbers');
+    document.getElementById('loading').style.display = 'none';
   }
 
 
-  //prevent the browser default behaviour
-  e.preventDefault();
+  
 }
 
 function showError(error){
